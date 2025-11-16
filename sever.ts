5674e1730b047896ef3220d9dd3f67a4b1e555b3
hendrix <amehharrison202017@gmail.com>
2025-11-16 07:31:20 +0000

    edited server

--- sever.ts
+++ sever.ts
@@ -1,2 +1,2 @@
-// export const backendUrl = "http://10.46.59.7:3000/";
+// export const backendUrl = git"http://10.46.59.7:3000/";
 export const backendUrl = "https://voxa-backend-diu5.onrender.com/";
